d2348eb0f1b89f75c8e36c41f3526b5d44ac2a03
unknown <ibr.s.ahmat@gmail.com>
2023-01-17 00:37:05 +0300

    Create the main pages

---
+++ pages/events/event.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const event = () => {
+  return <div>single event page</div>;
+};
+
+export default event;
---
+++ pages/events/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const events = () => {
+  return (
+    <div>
+      <h2>Events List Page</h2>
+      <div>
+        <a href="">
+          <div>
+            <img />
+            <h2>Events in Jeddah</h2>
+          </div>
+        </a>
+        <a href="">
+          <div>
+            <img />
+            <h2>Events in Riyadh</h2>
+          </div>
+        </a>
+        <a href="">
+          <div>
+            <img />
+            <h2>Events in Dubai</h2>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default events;
