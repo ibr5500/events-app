0c058365d48d09a19076c9ae4d7d92b73554a611
unknown <ibr.s.ahmat@gmail.com>
2023-01-19 00:36:11 +0300

    Make events per city page dynamic

---
+++ pages/events/[cat]/index.js
@@ -0,0 +1,64 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "@/styles/Home.module.css";
+
+const EventCategory = ({ data, pageName }) => {
+  return (
+    <div>
+      <h1>Events in {pageName}</h1>
+      <div>
+        {data.map((ev) => (
+          <Link
+            key={ev.id}
+            href={`/events/${ev.city}/${ev.id}`}
+            className={styles.cards}
+          >
+            <Image
+              width={200}
+              height={200}
+              src={ev.image}
+              alt={ev.title}
+            />
+            <h2>{ev.title}</h2>
+            <p>{ev.description}</p>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default EventCategory;
+
+export async function getStaticPaths() {
+  const { events_categories } = await import("/data/data.json");
+
+  const allPaths = events_categories.map((ev) => {
+    return {
+      params: {
+        cat: ev.id.toString(),
+      },
+    };
+  });
+  console.log(allPaths);
+  return {
+    paths: allPaths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  console.log(context);
+  const id = context?.params.cat;
+
+  const { allEvents } = await import("/data/data.json");
+
+  const data = allEvents.filter((ev) => ev.city === id);
+
+  return {
+    props: {
+      data,
+      pageName: id,
+    },
+  };
+}
