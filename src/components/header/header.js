b355d665cc402c5c246a3f98fb3042dc31fe72aa
unknown <ibr.s.ahmat@gmail.com>
2023-01-22 17:57:58 +0300

    Add styles for the header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,29 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const Header = () => (
   <header>
-    <nav>
-      {/*<img />*/}
-      <Link href="/">Home</Link>
-      <Link href="/events">Events</Link>
-      <Link href="/about">About</Link>
-    </nav>
+    <div className="navContiner">
+      <Image
+        src={"/images/next.svg"}
+        width={50}
+        height={50}
+        alt="logo"
+      />
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/events">Events</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   </header>
 );
 
